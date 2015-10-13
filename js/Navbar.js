4ab6f73c0f8feea1505aa9ea575ee23711450c6c
aranajhonny <jhonnyjosearana@gmail.com>
2015-10-13 16:22:38 -0430

    actualizando

--- js/Navbar.js
+++ js/Navbar.js
@@ -3,9 +3,9 @@
         var scroll = $(this).scrollTop();
         var topDist = $("#nav").position();
         if (scroll > topDist.top) {
-            $('nav').css({"position":"fixed","top":"0"});
+            $('nav').css({"position":"fixed","top":"0","opacity": "0.75"});
         } else {
-            $('nav').css({"position":"static","top":"auto"});
+            $('nav').css({"position":"static","top":"auto","opacity": "1"});
         }
     });
 });
